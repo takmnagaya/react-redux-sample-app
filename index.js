eac5632f42fa3ff1d1ce23834f456834559232cd
takmnagaya <takmnagaya@gmail.com>
2017-10-31 23:33:46 +0900

    Add TODO actions

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import { addTodo, toggleTodo, setVIsibilityFilter } from './actions/index.js';
 
-let store = createStore(function () { return 'Hello, Redux!' })
+let store = createStore(() => { return 'Hello' });
 
-var contents = document.getElementById('contents')
-contents.innerHTML = store.getState().toString()
+var addTodoElem = document.getElementById('addTodo');
+var input = addTodoElem.getElementsByTagName('input')[0];
+var button = addTodoElem.getElementsByTagName('button')[0];
+button.addEventListener('click', () => {
+    // ボタンをクリックしたら「TODOを追加する」というアクションをStoreに渡す
+    var todoText = input.value;
+    store.dispatch(addTodo(todoText));
+});
+
+// TODOの完了
+var todoList = document.getElementById('todoList');
+var elements = todoList.getElementsByTagName('li');
+var listArray = [...elements];
+listArray.forEach((v, index) => {
+    v.addEventListener('click', e => {
+        store.dispatch(toggleTodo(index));
+});
+});
+
+var links = document.getElementById('links');
+var childs = links.childNodes;
+var childList = [...childs];
+childList.filter(v => v.nodeName != '#text').forEach(v => {
+   v.addEventListener('click', e => {
+       var filterText = v.innerHTML;
+       store.dispatch(setVIsibilityFilter(filterText));
+});
+});
